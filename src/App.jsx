4ab4b9ca125d974fa610a5b89f5c5ed775d4bbcc
Paweł Zajączkowski <pawel.zajaczkowski91@gmail.com>
2024-04-19 17:01:40 +0200

    Added labels for name, surname and date, added button to generate random values

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { DESTINATIONS } from "./constants/DESTINATIONS";
 import "./App.css";
+import { useState } from "react";
 function App() {
+  const [abbr, setAbbr] = useState(() =>
+    losoweWartosci(DESTINATIONS, 20, "abbreviation")
+  );
+  const [exp, setExp] = useState(() =>
+    losoweWartosci(DESTINATIONS, 20, "expansion")
+  );
+
   function losoweWartosci(tablica, ilosc, klucz) {
     // Sprawdź, czy tablica jest pusta lub ilosc jest mniejsza od 0
     if (!tablica.length || ilosc <= 0) {
@@ -32,30 +40,40 @@ function App() {
     return wylosowaneWartosci;
   }
 
-  const randomAbbr = losoweWartosci(DESTINATIONS, 20, "abbreviation");
-  const randomExp = losoweWartosci(DESTINATIONS, 20, "expansion");
+  function handleGenerate() {
+    setAbbr(() => losoweWartosci(DESTINATIONS, 20, "abbreviation"));
+    setExp(() => losoweWartosci(DESTINATIONS, 20, "expansion"));
+  }
 
   return (
-    <>
-      <div className="wrapper">
+    <div className="wrapper">
+      <div className="list">
+        <div className="name">
+          <p>Imię: ....................</p>
+          <p>Nazwisko: ....................</p>
+          <p>Data: ....................</p>
+        </div>
         <h2>
           1. Napisz, które destynacje są w strefie Schengen, a które w
           Non-Schengen, oraz rozwiń skróty lotnisk
         </h2>
         <ul>
-          {randomAbbr.map((abbr) => (
+          {abbr.map((abbr) => (
             <li key={abbr}>{abbr}</li>
           ))}
         </ul>
         <h2>2. Zapisz skróty lotnisk według kodów IATA + Państwo</h2>
         <ul>
-          {randomExp.map((exp) => (
+          {exp.map((exp) => (
             <li key={exp}>{exp}</li>
           ))}
         </ul>
       </div>
-      <button onClick={() => window.print()}>DRUKUJ</button>
-    </>
+      <div>
+        <button onClick={() => window.print()}>DRUKUJ</button>
+        <button onClick={handleGenerate}>GENERUJ</button>
+      </div>
+    </div>
   );
 }
 
