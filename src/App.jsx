53b25fe4e0162dc0d5ff7a39bc585a18ca37955c
Paweł Zajączkowski <pawel.zajaczkowski91@gmail.com>
2024-08-06 16:58:24 +0200

    Update - 1. Added select to choose between KRK and KTW airport. 2. Added destinations for KRK. 3. Added favicon for APP. 4. Fixed CSS for printing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,52 @@
-import { DESTINATIONS } from "./constants/DESTINATIONS";
+import { DEST_KTW, DEST_KRK } from "./constants/DESTINATIONS";
 import "./App.css";
 import { useState } from "react";
 function App() {
+  const [selectedAirport, setSelectedAirport] = useState("KTW");
   const [abbr, setAbbr] = useState(() =>
-    losoweWartosci(DESTINATIONS, 20, "abbreviation")
+    losoweWartosci(selectedAirport, 20, "abbreviation")
   );
   const [exp, setExp] = useState(() =>
-    losoweWartosci(DESTINATIONS, 20, "expansion")
+    losoweWartosci(selectedAirport, 20, "expansion")
   );
 
-  function losoweWartosci(tablica, ilosc, klucz) {
-    // Sprawdź, czy tablica jest pusta lub ilosc jest mniejsza od 0
+  function losoweWartosci(selectedAirport, ilosc, klucz) {
+    const tablica = selectedAirport === "KTW" ? DEST_KTW : DEST_KRK;
     if (!tablica.length || ilosc <= 0) {
       return [];
     }
 
-    // Utwórz nową pustą tablicę na wylosowane wartości
     const wylosowaneWartosci = [];
 
-    // Skopiuj tablicę oryginalną do pomocniczej
     const pomocniczaTablica = [...tablica];
 
-    // Pętla do wylosowania określonej liczby wartości
     for (let i = 0; i < ilosc; i++) {
-      // Wylosuj losowy indeks w zakresie pozostałych elementów tablicy
       const losowyIndeks = Math.floor(Math.random() * pomocniczaTablica.length);
 
-      // Pobierz wartość z klucza obiektu
       const wartosc = pomocniczaTablica[losowyIndeks][klucz];
 
-      // Dodaj wylosowaną wartość do nowej tablicy
       wylosowaneWartosci.push(wartosc);
 
-      // Usuń wylosowany obiekt z tablicy pomocniczej, aby uniknąć duplikatów
       pomocniczaTablica.splice(losowyIndeks, 1);
     }
-
-    // Zwróć tablicę z wylosowanymi wartościami
     return wylosowaneWartosci;
   }
 
   function handleGenerate() {
-    setAbbr(() => losoweWartosci(DESTINATIONS, 20, "abbreviation"));
-    setExp(() => losoweWartosci(DESTINATIONS, 20, "expansion"));
+    setAbbr(() => losoweWartosci(selectedAirport, 20, "abbreviation"));
+    setExp(() => losoweWartosci(selectedAirport, 20, "expansion"));
   }
 
   return (
     <div className="wrapper">
       <div>
+        <select
+          onChange={(e) => setSelectedAirport(e.target.value)}
+          defaultValue="KTW"
+        >
+          <option value="KTW">KTW</option>
+          <option value="KRK">KRK</option>
+        </select>
         <button onClick={() => window.print()}>DRUKUJ</button>
         <button onClick={handleGenerate}>GENERUJ</button>
       </div>
